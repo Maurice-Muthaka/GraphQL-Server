432e0f0789171581c70c499eb04010cddd820ee9
Maurice Muthaka <mauricemuthaka@gmail.com>
2022-08-01 04:56:26 +0300

    graphQL playground

--- build/index.js
+++ build/index.js
@@ -1,13 +1,15 @@
 "use strict";
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const expressPlayground = require("graphql-playground-middleware-express").default;
 const { typeDefs } = require("./Schema/TypeDefs");
 const { resolvers } = require("./Schema/Resolvers");
-const { ApolloServer } = require("apollo-server-express");
-const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 const server = new ApolloServer({ typeDefs, resolvers });
 server.start().then(() => {
     server.applyMiddleware({ app });
+    app.get("/", expressPlayground({ endpoint: "/graphql" }));
     app.listen({ port: port }, () => {
         console.log(`Server running on port ${port}`);
     });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const expressPlayground = require("graphql-playground-middleware-express").default;
+
 const { typeDefs } = require("./Schema/TypeDefs");
 const { resolvers } = require("./Schema/Resolvers")
-const { ApolloServer } = require("apollo-server-express");
-const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3001
@@ -10,6 +12,8 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 server.start().then(() => {
     server.applyMiddleware({ app });
+
+    app.get("/", expressPlayground({ endpoint: "/graphql" }));
 
     app.listen({ port: port }, () => {
         console.log(`Server running on port ${port}`);
